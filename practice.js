b4156700fc621ad8f7f8e8722e75473d05695319
Masongriff1 <masongriffith22@gmail.com>
2023-09-20 13:10:15 -0700

    fixed 2.1 assignment

---
+++ practice.js
@@ -0,0 +1,17 @@
+        //create a variable to hold favorite food type
+        var favFoodType = "Shrimp";
+
+
+        /*to create a function start with the function keyword
+          give the function a description name - usually an action
+          () - if the function needs value(s) put that/them in between the ()
+          make sure the character after the () is a {
+              close out the function with a }
+        */
+        function displayFavFood(favFoodType){
+            //call the built in java script alert fuction; this will cause a pop up to a display with the
+            //text that we pass in
+            alert(favFoodType);
+        }
+        //call the function we just created, pass in the variable with our favorite food type
+        displayFavFood(favFoodType);
