580aa0727d06b37f75d59803b23b6e96073a0815
Masongriff1 <masongriffith22@gmail.com>
2023-12-10 23:09:52 -0700

    finished 7.1

--- eventExamples.js
+++ eventExamples.js
@@ -1,15 +1,61 @@
 function addButton() {
-    var inputElement = document.createElement("input");
-    inputElement.setAttribute("type", "button");
-    inputElement.setAttribute("value","New Button");
-    // var buttonText = document.createTextNode("New Button");
-    // inputElement.appendChild(buttonText);
+    console.log("Adding Start and Stop Buttons");
 
-    var parent = document.getElementById("memeParagraph");
-    parent.appendChild(inputElement);
+    let inputElementStart = document.createElement("input");
+    inputElementStart.setAttribute("type", "button");
+    inputElementStart.setAttribute("value", "Start");
+    inputElementStart.setAttribute("id", "startButton");
+    inputElementStart.setAttribute("onclick", "startButton();");
 
-    var newParagraph = document.createElement("p");
-    newParagraph.appendChild(paragraphText),
+    let inputElementStop = document.createElement("input");
+    inputElementStop.setAttribute("type", "button");
+    inputElementStop.setAttribute("value", "Stop");
+    inputElementStop.setAttribute("id", "stopButton");
+    inputElementStop.setAttribute("onclick", "stopButton();");
+    inputElementStop.setAttribute("disabled", "true");
 
-    parent.appendChild(newParagraph);
+    let parent = document.getElementById("memeParagraph")
+    parent.appendChild(inputElementStart);
+    parent.appendChild(inputElementStop);
 }
+
+function startButton() {
+    console.log("pressed Start!");
+    let inputElementStart = document.getElementById("startButton");
+    inputElementStart.setAttribute("disabled", "true");
+
+    let inputElementStop = document.getElementById("stopButton");
+    inputElementStop.removeAttribute("disabled");
+
+    let memeImage=document.getElementById("memeImage");
+    memeImage.style.animationDuration = "3s";
+}
+
+function stopButton() {
+    console.log("Pressed Stop!");
+
+    let inputElementStart = document.getElementById("startButton");
+    inputElementStart.removeAttribute("disabled");
+
+    let inputElementStop = document.getElementById("stopButton");
+    inputElementStop.setAttribute("disabled", "true");
+    memeImage.style.animationDuration = "0s";
+}
+
+
+
+    // var inputElement = document.createElement("input");
+    // inputElement.setAttribute("type", "button");
+    // inputElement.setAttribute("value","New Button");
+    // // var buttonText = document.createTextNode("New Button");
+    // // inputElement.appendChild(buttonText);
+
+    // var parent = document.getElementById("memeParagraph");
+    // parent.appendChild(inputElement);
+
+    // var newParagraph = document.createElement("p");
+
+    // var paragraphText = document.createTextNode("New Paragraph");
+    // newParagraph.appendChild(paragraphText);
+
+    // parent.appendChild(newParagraph);
