4bd126037521d31b066e4926027dbc2e9e304f54
Masongriff1 <masongriffith22@gmail.com>
2023-12-14 13:33:17 -0700

    working on strings

--- validateInput.js
+++ validateInput.js
@@ -1,3 +1,33 @@
+class polygon {
+    constructor (sides) {
+        this.sides = sides;
+    }
+
+    alertSides(){
+        alert("this polygon has " + this.sides);
+    }
+
+    alertFormula(){
+        alert("The area of this object is...");
+    }
+}
+
+class square extends polygon{
+    constructor (length){
+        super(4);
+        this.length = length;
+    }
+
+    area(){
+        alert(this.length * this.length);
+    }
+
+    areaFormula(){
+        super.alertFormula();
+        alert("... length squared.");
+    }
+}
+
 function charAtDemo() {
     var someText = fname.value;
     var lastLetter = someText.charAt(someText.length - 1);
@@ -35,4 +65,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
     if(form) {
         form.addEventListener("submit", validateInput);
     }
+
+    var poly = new polygon(8);
+    poly.alertFormula();
+    poly.alertSides();
+
+    var square = new square(3);
+    square.alertFormula(); // polymorphism via method overriding
+    square.area();
 });
